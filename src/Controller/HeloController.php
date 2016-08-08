f616c4b43255e1e52d123fb4b5647de76b3c8b32
vagrant <vagrant@debian>
2016-08-08 11:46:48 +0900

    edit

--- src/Controller/HeloController.php
+++ src/Controller/HeloController.php
@@ -5,28 +5,10 @@ use App\Controller\AppController;
  
 class HeloController extends AppController
 {
-	    public function index($a = '')
-				    {
-                      //$this->set('message', 'Hello! this is sample page. ;-)');
-							        if ($a == '') {
-												$this->setAction('err');
-												return;
-											}
-							        $this->autoRender = false;
-										  echo "<html><head></head><body>";
-										  echo "<h1>Hello!</h1>";
-											echo "<p>これは、サンプルで作成したページです。</p><p>";
-											if ($a != '') {
-											    echo " パラメータA: " . $a;
-											}
-											echo "</p></body></html>";
-		      	}
-            public function err()
-						{
-							$this->autoRender = false;
-							echo "<html><head></head><body>";
-							echo "<h1>Hello!</h1>";
-							echo "<p>パラメータがありませんでした。</p>";
-							echo "</body></html>";
-						}
+	  public function index()
+		{
+			$id = $this->request->query('id');
+			$name = $this->request->query('name');
+			$this->set('message', 'your id:' . $id . ', name:' . $name);
+		}
 }
